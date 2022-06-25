fcbb50d7cd162f9b930b33b9837d7fa325412931
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-25 12:59:56 +0300

    Added Basic generator logic

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-if __name__ =='__main__':
-    print("hello")
+nested_list = [
+    ['a',  ['w', 1, 'w'], 'b', 'c'],
+    ['d', 'e', [1,7,['z','y'],8,4],'f', 'h', False],
+    [1, 2, None],
+]
+
+
+def generator(iter):
+    my_list = []
+    cursor = 0
+    for item in iter[::-1]:
+        if type(item) == list:
+            while item:
+                e = item.pop()
+                if type(e) == list:
+                    item.extend(e)
+                else:
+                    my_list.append(e)
+    my_list.reverse()
+    while cursor < len(my_list):
+        yield my_list[cursor]
+        cursor += 1
+
+
+if __name__ == '__main__':
+    for i in generator(nested_list):
+        print(i)
